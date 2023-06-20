2a2024990cca19d10137230d2e41806e0e1930dc
MMNikolov <m.m.nikolov.2006@gmail.com>
2023-06-20 22:02:01 +0300

    Adding Inheritance

---
+++ C# OOP/Inheritance/CustomStack/StackOfStrings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string> 
+    {
+        Stack<string> stack = new();
+
+        public bool IsEmpty()
+        {
+            if (stack.Any())
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public Stack<string> AddRange(IEnumerable<string> range)
+        {
+            foreach (var item in range)
+            {
+                Push(item);
+            }
+
+            return this;
+        }
+    }
+}
---
+++ C# OOP/Inheritance/Farm/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+
+            animal.Eat();
+
+            Dog dog = new();
+
+            dog.Eat();
+
+            dog.Bark();
+        }
+    }
+}
