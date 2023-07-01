2c9f9ad7c731d85568d1a367136c2182b1224146
MMNikolov <m.m.nikolov.2006@gmail.com>
2023-07-01 16:57:02 +0300

    Adding Encapsulation EX

---
+++ C# OOP/EncapsulationEX/ClassBoxData/Box.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+      
+        public double Length
+        {
+            get { return length; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get { return width; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                height = value;
+            }
+        }
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double SurfaceArea()
+        => 2 * (length * height + width * height + width * length);
+
+        public double LateralSurfaceArea()
+        => 2 * (width * height + height * length);
+
+        public double Volume()
+        => length * width * height;
+
+        public override string ToString()
+        => $"Surface Area - {SurfaceArea():f2}" + Environment.NewLine +
+           $"Lateral Surface Area - {LateralSurfaceArea():f2}" + Environment.NewLine +
+           $"Volume - {Volume():f2}";
+
+    }
+}
---
+++ C# OOP/EncapsulationEX/PizzaCalories/Program.cs
@@ -0,0 +1,45 @@
+﻿using PizzaCalories.Model;
+
+namespace PizzaCalories
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string nameOfPizza = Console.ReadLine()
+                    .Split()[1];
+
+                string[] dataForDough = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                Dough dough = new Dough(dataForDough[1], dataForDough[2], double.Parse(dataForDough[3]));
+                Pizza pizza = new Pizza(nameOfPizza, dough);
+
+                while (true)
+                {
+                    string input = Console.ReadLine();
+
+                    if (input == "END")
+                    {
+                        break;
+                    }
+
+                    string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                    Toppings topping = new Toppings(tokens[1], double.Parse(tokens[2]));
+
+                    pizza.AddTopping(topping);
+                }
+
+                Console.WriteLine(pizza);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
