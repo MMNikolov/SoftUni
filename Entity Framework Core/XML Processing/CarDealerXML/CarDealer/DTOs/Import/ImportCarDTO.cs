27b4f5abf7e728a8480c692cd11a55b122e3db99
MMNikolov <m.m.nikolov.2006@gmail.com>
2023-11-27 17:12:24 +0200

    Adding lecturers answers to exam prep

---
+++ Entity Framework Core/XML Processing/CarDealerXML/CarDealer/DTOs/Import/ImportCarDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Import
+{
+    [XmlType("Car")]
+    public class ImportCarDTO
+    {
+        [XmlElement("make")]
+        public string Make { get; set; }
+
+        [XmlElement("model")]
+        public string Model { get; set; }
+        [XmlElement("traveledDistance")]
+        public long TraveledDistance { get; set; }
+
+        [XmlArray("parts")]
+        public ImportPartIdDTO[] PartsIds { get; set; }
+    }
+}
---
+++ Entity Framework Core/XML Processing/CarDealerXML/CarDealer/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace CarDealer.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString =
+            @"Server=DESKTOP-PP7HKGS\SQLEXPRESS;Database=CarDealer;Integrated Security=True;";
+    }
+}
