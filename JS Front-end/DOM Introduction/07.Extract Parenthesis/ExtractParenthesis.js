d72fb61c115b8b0a2320bc475cd25606a5e3cb99
MMNikolov <m.m.nikolov.2006@gmail.com>
2024-03-29 19:33:30 +0200

    .

--- JS Front-end/DOM Introduction/07.Extract Parenthesis/ExtractParenthesis.js
+++ JS Front-end/DOM Introduction/07.Extract Parenthesis/ExtractParenthesis.js
@@ -3,9 +3,11 @@ function extract(content) {
     let pattern = /\(([^)]+)\)/g;
     let result = [];
     let match = pattern.exec(para);
+
     while(match) {
-    result.push(match[1]);
-    match = pattern.exec(para);
+        result.push(match[1]);
+        match = pattern.exec(para);
     }
+    
     return result.join('; ');
 }
