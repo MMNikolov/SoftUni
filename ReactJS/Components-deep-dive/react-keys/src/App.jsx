7b7a137dc718c6f6d3db02d0f0c2e8ad12ad2cc6
MMNikolov <m.m.nikolov.2006@gmail.com>
2024-07-12 00:50:30 +0300

    .

---
+++ ReactJS/Components-deep-dive/react-keys/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  const [movies, setMovies] = useState([
+    'The Matrix',
+    "Man of steel",
+    'Lord of the rings'
+  ])
+
+  const buttonClickHandler = () => {
+    setMovies((oldMovies) => {
+      const newMovies = [...oldMovies]
+      newMovies[2] = 'Harry Potter'
+
+      return newMovies
+    })
+  }
+  
+  return (
+    <>
+      <h1>Movies</h1>
+      <ul>
+        {movies.map(movie => <li key={movie}>{movie}</li>)}
+      </ul>
+
+      <button onClick={buttonClickHandler}>Change</button>
+    </>
+  )
+}
+
+export default App
