1b69e5a3a7217ee465a46e1fbb60a5a58604fe34
MMNikolov <m.m.nikolov.2006@gmail.com>
2024-07-18 00:51:51 +0300

    .

---
+++ ReactJS/Workshop-components/client/src/App.jsx
@@ -0,0 +1,20 @@
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import "./styles.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <main className="main">
+
+        
+      </main>
+
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
