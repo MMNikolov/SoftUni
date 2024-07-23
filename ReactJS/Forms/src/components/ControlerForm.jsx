2a1be3e9f58043de0292176542ed390bad68abba
MMNikolov <m.m.nikolov.2006@gmail.com>
2024-07-23 20:15:16 +0300

    .

---
+++ ReactJS/Forms/src/components/ControlerForm.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+
+export default function ControlledForm(){
+    const [user, setUser] = useState({})
+
+    const formSubmitHandler = (e) => {
+        e.PreventDefault()
+
+        const formData = new FormData(e.currentTarget)
+
+        setUser({
+            username: formData.get('username')
+        })
+    }
+    
+    return(
+        <>
+            <h1>Controlled Form</h1>
+
+            {user.username
+                ? <p>Hello {user.username}!</p>
+                :(<form onSubmit={formSubmitHandler}>
+                    <div>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" name="username" id="username" />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id="password" />
+                    </div>
+                    <div>
+                        <input type="submit" value="Login" />
+                    </div>
+                </form>)}
+        </>
+    )
+}
